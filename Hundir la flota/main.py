6bf217c00e4d71ae1260a60bbfa217111f2d8344
hug4470 <hugomenendez4470@gmail.com>
2024-09-27 09:20:12 +0200

    m

--- Hundir la flota/main.py
+++ Hundir la flota/main.py
@@ -2,6 +2,20 @@ import numpy as np
 import random
 import utils
 
+print("mapas de prueba")
+
+utils.crearaleatorios
+
+utils.crearaleatorios
+
+utils.crearaleatorios
+
+utils.crearaleatorios
+
+utils.crearaleatorios
+
+print ("fin de la prueba")
+
 utils.iniciador
 
 utils.turnos
